6e6eabd7334979c79ada208d82f1653a2cfb2566
parkcrystal <9jeworld@naver.com>
2020-05-31 12:02:44 +0900

    어댑터 상속

--- app/src/main/java/com/phis/jickbangcopy_20200531/adapters/BangAdapter.kt
+++ app/src/main/java/com/phis/jickbangcopy_20200531/adapters/BangAdapter.kt
@@ -1,5 +1,13 @@
 package com.phis.jickbangcopy_20200531.adapters
 
-class BangAdapter {
+import android.content.Context
+import android.service.voice.VoiceInteractionSession
+import android.view.autofill.AutofillId
+import android.widget.ArrayAdapter
+import com.phis.jickbangcopy_20200531.datas.Room
+
+class BangAdapter(context: Context,  resId: Int, list:List<Room>  ) : ArrayAdapter<Room> (context,resId, list){
+
+
 
 }
