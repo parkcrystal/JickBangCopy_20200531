a8b17a760abeece8e982e071388fdebaed2def4f
parkcrystal <9jeworld@naver.com>
2020-05-31 12:09:09 +0900

    코드 정리용 클래스 - BaseActivity

---
+++ app/src/main/java/com/phis/jickbangcopy_20200531/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.phis.jickbangcopy_20200531
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract  class  BaseActivity : AppCompatActivity(){
+
+    val mContext =  this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
+
